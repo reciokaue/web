16dc7673ecd512024ccab05e275c0d1a5ed5f4df
Kaue Recio Carpim Sala <kaue.recio2@gmail.com>
2023-05-19 12:14:33 -0300

    callback route and save token on cookies

---
+++ src/app/api/auth/callback/route.ts
@@ -0,0 +1,21 @@
+import { api } from '@/services/api'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url)
+  const code = searchParams.get('code')
+
+  const registerResponse = await api.post('/register', {
+    code,
+  })
+
+  const { token } = registerResponse.data
+
+  const redirectUrl = new URL('/', request.url)
+
+  return NextResponse.redirect(redirectUrl, {
+    headers: {
+      'Set-Cookie': `token=${token}; Path=/; max-age=${60 * 60 * 24 * 30}`,
+    },
+  })
+}

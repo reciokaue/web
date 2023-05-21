c11133aee8d8a92ebe198b5e5e87f5ac84fbe7b2
Kaue Recio Carpim Sala <kaue.recio2@gmail.com>
2023-05-21 16:19:38 -0300

    next route authentication

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -4,18 +4,23 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
+  const redirectTo = request.cookies.get('redirectTo')?.value
 
-  const registerResponse = await api.post('/register', {
-    code,
-  })
+  try {
+    const registerResponse = await api.post('/register', {
+      code,
+    })
 
-  const { token } = registerResponse.data
+    const { token } = registerResponse.data
+    const redirectUrl = redirectTo ?? new URL('/', request.url)
 
-  const redirectUrl = new URL('/', request.url)
-
-  return NextResponse.redirect(redirectUrl, {
-    headers: {
-      'Set-Cookie': `token=${token}; Path=/; max-age=${60 * 60 * 24 * 30}`,
-    },
-  })
+    return NextResponse.redirect(redirectUrl, {
+      headers: {
+        'Set-Cookie': `token=${token}; Path=/; max-age=${60 * 60 * 24 * 30}`,
+      },
+    })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.next()
+  }
 }
